ea9e2b5665bce940f578e0611c59e330b3359c8e
Mohit Lalasaheb  Kate <mk21314@corp.ojas-it.com>
2021-10-26 17:16:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ListEmployeeComponent from './Components/ListEmployeeComponent';
+import HeaderComponent from './Components/HeaderComponent';
+import FooterComponent from './Components/FooterComponent';
+import ViewEmployeeComponent from './Components/ViewComponent';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateEmployeeCompo from './Components/CreateEmployeeCompo';
+import Registration from './Components/Registration';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <Route
+              path="/add-employee/:id"
+              component={CreateEmployeeCompo}
+            ></Route>
+            <Route
+              path="/view-employee/:id"
+              component={ViewEmployeeComponent}
+            ></Route>
+
+
+             {/* <Route
+              path="/Registration"
+              component={Registration}
+            ></Route> */}
+          </Switch>
+        </div>
+        <div style={{textAlign:"center"}}>
+           <FooterComponent />
+           </div>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/FooterComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import './FooterComponent.css';
+class FooterComponent extends Component {
+ 
+  render() {
+    return (
+      <div>
+          <hr></hr>
+        <footer id="foo">
+          <span >
+            All Rights Reserved 2021 @Ojas – it Trainers
+          </span>
+        </footer>
+      </div>
+    );
+  }
+}
+export default FooterComponent;
